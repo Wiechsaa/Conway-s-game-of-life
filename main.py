51dc69767fe669553facfabb178fff338e950e4b
Wiechsaa <98656443+Wiechsaa@users.noreply.github.com>
2022-01-29 21:48:29 +0100

    Minor changes

--- main.py
+++ main.py
@@ -4,36 +4,37 @@ from grid import Grid
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-width, height = 1920, 1080
-size = (width, height)
+# Settings
+WIDTH, HEIGHT = 1920, 1080
+SIZE = (WIDTH, HEIGHT)
+FPS = 60
+
+BLACK = (0, 0, 0)
+BLUE = (0,0,255)
+WHITE = (255, 255, 255)
+
+SCALER = 20
+OFFSET = 1
 
 pygame.init()
 pygame.display.set_caption(' Conway game of life')
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
-fps = 60
 
-black = (0, 0, 0)
-blue = (0,0,255)
-white = (255, 255, 255)
-
-scaler = 20
-offset = 1
-
-board = Grid(width, height, scaler, offset)
+board = Grid(WIDTH, HEIGHT, SCALER, OFFSET)
 board.random_2d_array()
 
     
 if __name__ == '__main__':
     run = True
     while run:
-        clock.tick(fps)
-        screen.fill(black)
+        clock.tick(FPS)
+        screen.fill(BLACK)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             
-        board.game(off_color=white, on_color=blue, surface=screen)           
+        board.game(off_color=WHITE, on_color=BLUE, surface=screen)           
     
         pygame.display.update()
